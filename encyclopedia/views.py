83f143fde494a5d64278993a367fd68d95e23a76
aegis945 <artiom.lekincev@gmail.com>
2024-10-14 16:38:13 +0300

    add converter, display requested entry, add path

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,30 @@
 from django.shortcuts import render
-
+from markdown2 import Markdown
 from . import util
-
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
+def convert_md_to_html(title):
+    content = util.get_entry(title)
+    markdowner = Markdown()
+
+    if content is None:
+        return None
+    else:
+        return markdowner.convert(content)
+
+def entry(request, title):
+    html_content = convert_md_to_html(title)
+    if html_content is None:
+        return render(request, "encyclopedia/error.html", {
+            "title": title
+        })
+    else:
+        return render(request, "encyclopedia/entry.html", {
+            "title": title,
+            "content": html_content
+        })
 
